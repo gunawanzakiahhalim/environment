e50b97dd6e25280d848f2f69454579b3980eafb6
gunawanzakiahhalim <gunawanzakiahhalim@gmail.com>
2021-01-28 16:00:21 +0700

    modified script.js

--- script.js
+++ script.js
@@ -1,113 +1,141 @@
 // 2. looping
+// 4. Nested 
+// HTML Fragment Bersarang
 const list = [{
-        judul: 'rensi',
-        link: 'https://rensi135.000webhostapp.com/'
+        kategori: 'Online Server',
+        links: [{
+                judul: 'rensi',
+                url: 'https://rensi135.000webhostapp.com/'
+            },
+            {
+                judul: 'redeem',
+                url: 'https://support135.000webhostapp.com/redeem/'
+            },
+            {
+                judul: 'pulau135',
+                url: 'https://pulau135.000webhostapp.com/'
+            },
+            {
+                judul: 'nc agent',
+                url: 'https://rekapnc.000webhostapp.com/'
+            },
+            {
+                judul: 'pelayanan pds',
+                url: 'https://apps1351.000webhostapp.com'
+            },
+            {
+                judul: 'chanelsosmed',
+                url: 'https://chanelsosmed.000webhostapp.com/'
+            },
+            {
+                judul: 'file pdf it dev',
+                url: 'https://pansolsosmed.000webhostapp.com/'
+            },
+            {
+                judul: 'eca',
+                url: 'https://ops4.000webhostapp.com/'
+            },
+            {
+                judul: 'pds135',
+                url: 'https://pds135.000webhostapp.com/'
+            },
+            {
+                judul: 'spbu',
+                url: 'https://ops6.000webhostapp.com/'
+            },
+            {
+                judul: 'pansol',
+                url: 'https://pansolwa.000webhostapp.com'
+            },
+            {
+                judul: 'nc bes',
+                url: 'https://ncbes.000webhostapp.com/'
+            },
+            {
+                judul: 'arjunadigital',
+                url: 'https://arjunadigital.000webhostapp.com/'
+            },
+            {
+                judul: 'survey',
+                url: 'https://ops10.000webhostapp.com/'
+            },
+            {
+                judul: 'pansol lama',
+                url: 'https://ops11.000webhostapp.com/'
+            },
+            {
+                judul: 'listpangkalan',
+                url: 'https://listpangkalan.000webhostapp.com/'
+            },
+        ],
     },
     {
-        judul: 'redeem',
-        link: 'https://support135.000webhostapp.com/redeem/'
+        kategori: 'Google',
+        links: [{
+                judul: 'drive',
+                url: 'https://drive.google.com/'
+            },
+            {
+                judul: 'keep',
+                url: 'https://keep.google.com/'
+            },
+            {
+                judul: 'youtube',
+                url: 'https://www.youtube.com/'
+            }
+        ]
     },
     {
-        judul: 'pulau135',
-        link: 'https://pulau135.000webhostapp.com/'
+        kategori: 'Microsoft',
+        links: [{
+            judul: 'onedrive',
+            url: 'https://onedrive.live.com/about/en-us/signin/'
+        }]
     },
     {
-        judul: 'nc agent',
-        link: 'https://rekapnc.000webhostapp.com/'
+        kategori: 'Web Server',
+        links: [{
+            judul: '000webhost',
+            url: 'https://www.000webhost.com/'
+        }]
     },
     {
-        judul: 'pelayanan pds',
-        link: 'https://apps1351.000webhostapp.com'
+        kategori: 'Version Control System',
+        links: [{
+            judul: 'github',
+            url: 'https://github.com/'
+        }]
     },
     {
-        judul: 'chanelsosmed',
-        link: 'https://chanelsosmed.000webhostapp.com/'
+        kategori: 'Facebook',
+        links: [{
+            judul: 'whatsapp',
+            url: 'https://web.whatsapp.com/'
+        }]
     },
     {
-        judul: 'file pdf it dev',
-        link: 'https://pansolsosmed.000webhostapp.com/'
+        kategori: 'Cloud',
+        links: [{
+                judul: 're-cloud',
+                url: 'https://re-cloud.id/'
+            },
+            {
+                judul: 'alibabacloud',
+                url: 'https://id.alibabacloud.com/'
+            }
+        ]
     },
     {
-        judul: 'eca',
-        link: 'https://ops4.000webhostapp.com/'
-    },
-    {
-        judul: 'pds135',
-        link: 'https://pds135.000webhostapp.com/'
-    },
-    {
-        judul: 'spbu',
-        link: 'https://ops6.000webhostapp.com/'
-    },
-    {
-        judul: 'pansol',
-        link: 'https://pansolwa.000webhostapp.com'
-    },
-    {
-        judul: 'nc bes',
-        link: 'https://ncbes.000webhostapp.com/'
-    },
-    {
-        judul: 'arjunadigital',
-        link: 'https://arjunadigital.000webhostapp.com/'
-    },
-    {
-        judul: 'survey',
-        link: 'https://ops10.000webhostapp.com/'
-    },
-    {
-        judul: 'pansol lama',
-        link: 'https://ops11.000webhostapp.com/'
-    },
-    {
-        judul: 'listpangkalan',
-        link: 'https://listpangkalan.000webhostapp.com/'
-    },
-    {
-        judul: 'drive',
-        link: 'https://drive.google.com/'
-    },
-    {
-        judul: 'keep',
-        link: 'https://keep.google.com/'
-    },
-    {
-        judul: 'youtube',
-        link: 'https://www.youtube.com/'
-    },
-    {
-        judul: 'onedrive',
-        link: 'https://onedrive.live.com/about/en-us/signin/'
-    },
-    {
-        judul: '000webhost',
-        link: 'https://www.000webhost.com/'
-    },
-    {
-        judul: 'github',
-        link: 'https://github.com/'
-    },
-    {
-        judul: 'whatsapp',
-        link: 'https://web.whatsapp.com/'
-    },
-    {
-        judul: 're-cloud',
-        link: 'https://re-cloud.id/'
-    },
-    {
-        judul: 'alibabacloud',
-        link: 'https://id.alibabacloud.com/'
-    },
-    {
-        judul: 'javascript Visualize',
-        link: 'http://pythontutor.com/javascript.html#mode=edit'
+        kategori: 'Javascript',
+        links: [{
+            judul: 'javascript Visualize',
+            url: 'http://pythontutor.com/javascript.html#mode=edit',
+        }]
     }
 ];
-const el = `<dl>
-<dt>Links</dt>
-${list.map(m => `<dd>
-<a href="${m.link}">${m.judul}</a>
-</dd>`).join('')}
-</dl>`;
+
+function cetakLink(links) {
+    return `${links.map(link =>`<dd><a href="${link.url}">${link.judul}</a></dd>`).join('')}`;
+}
+const el = `<div class="list">${list.map(m =>`<dl><dt>${m.kategori}</dt>${cetakLink(m.links)}</dl>`).join('')}</div>`;
 document.body.innerHTML = el;
