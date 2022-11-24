24ba9cba4982fb576f09c6a57432afa54e4810d0
gunawanzakiahhalim <gunawanzakiahhalim@gmail.com>
2022-11-24 13:42:45 +0700

    update uri figma

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ const list = [
     kategori: 'Session',
     links: [
       {
+        judul: 'figma',
+        url: 'https://www.figma.com/',
+      },
+    ],
+  },
+  {
+    kategori: 'Session',
+    links: [
+      {
         judul: 'batas-waktu-utk-session',
         url: 'https://sekolahkoding.com/forum/cara-memberikan-batas-waktu-utk-session-user-codeigniter',
       },
